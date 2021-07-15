129c81759260103f3707a61236b351ee6ab1f68c
Rudolf <spureszka@gmail.com>
2021-07-15 15:56:44 +0200

    Console Preview

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Create the required arrays
 let squareOne = []
 let squareTwo = []
 let squareThree = []
@@ -8,12 +9,15 @@ let squareSeven = []
 let squareEight = []
 let squareNine = []
 
+// restarter parameters (No need to touch it. If generation is unsolvable, it will restart)
 const tryLimit = 1000;
 let stopper = 0;
 let exit = 0;
 
+// Generate 3x3 squares here.
 async function createNineSquare() {
     try {
+        //Start 3x3 generate (Each subsequent square adapts vertically and horizontally during generation)
         await createSquareOne()
         await createSquareTwo()
         await createSquareThree()
@@ -23,29 +27,35 @@ async function createNineSquare() {
         await createSquareSeven()
         await createSquareEight()
         await createSquareNine()
+        // Generate end
+
+        // Shows the console generated sudoku board (Can be deleted)
         console.log("Successful generation! New Sudoku Board");
-        console.log("Square One Numbers " + squareOne)
-        console.log("Square Two Numbers " + squareTwo)
-        console.log("Square Three Numbers " + squareThree)
-        console.log("Square Four Numbers " + squareFour)
-        console.log("Square Five Numbers " + squareFive)
-        console.log("Square Six Numbers " + squareSix)
-        console.log("Square Seven Numbers " + squareSeven)
-        console.log("Square Eight Numbers " + squareEight)
-        console.log("Square Nine Numbers " + squareNine)
+        console.log("Preview");
+        console.log("--------------------")
+        console.log(squareOne[0],squareOne[1],squareOne[2],squareTwo[0],squareTwo[1],squareTwo[2],squareThree[0],squareThree[1],squareThree[2])
+        console.log(squareOne[3],squareOne[4],squareOne[5],squareTwo[3],squareTwo[4],squareTwo[5],squareThree[3],squareThree[4],squareThree[5])
+        console.log(squareOne[6],squareOne[7],squareOne[8],squareTwo[6],squareTwo[7],squareTwo[8],squareThree[6],squareThree[7],squareThree[8])
+        console.log(squareFour[0],squareFour[1],squareFour[2],squareFive[0],squareFive[1],squareFive[2],squareSix[0],squareSix[1],squareSix[2])
+        console.log(squareFour[3],squareFour[4],squareFour[5],squareFive[3],squareFive[4],squareFive[5],squareSix[3],squareSix[4],squareSix[5])
+        console.log(squareFour[6],squareFour[7],squareFour[8],squareFive[6],squareFive[7],squareFive[8],squareSix[6],squareSix[7],squareSix[8])
+        console.log(squareSeven[0],squareSeven[1],squareSeven[2],squareEight[0],squareEight[1],squareEight[2],squareNine[0],squareNine[1],squareNine[2])
+        console.log(squareSeven[3],squareSeven[4],squareSeven[5],squareEight[3],squareEight[4],squareEight[5],squareNine[3],squareNine[4],squareNine[5])
+        console.log(squareSeven[6],squareSeven[7],squareSeven[8],squareEight[6],squareEight[7],squareEight[8],squareNine[6],squareNine[7],squareNine[8])
+        console.log("--------------------")
+        // Preview end 
     } catch {
-        console.log("Generation failed. Try again")
+        // If it cannot be generated successfully. It will start again
         return restart()
     }
 }
 
-createNineSquare()
+createNineSquare()  
 
+// Generate a random Number 1-9
 function randomNumbers() {
     return new Promise((resolve, reject) => {
-        const newRandomNumber = Math.random() * 9;
-        const ceilRandomNumber = Math.ceil(newRandomNumber)
-        resolve(ceilRandomNumber)
+        resolve(Math.ceil(Math.random() * 9))
     })
 }
 
@@ -97,7 +107,6 @@ function createSquareTwo() {
                         return resolve(createSquareTwo())
                     }
                     if (i === 9) {
-                        i++
                         resolve()
                     }
                 }))
@@ -136,7 +145,6 @@ function createSquareThree() {
                         return resolve(createSquareThree())
                     }
                     if (i === 9) {
-                        i++
                         resolve()
                     }
                 }))
@@ -175,7 +183,6 @@ function createSquareFour() {
                         return resolve(createSquareFour())
                     }
                     if (i === 9) {
-                        i++
                         resolve()
                     }
                 }))
@@ -214,7 +221,6 @@ function createSquareFive() {
                         return resolve(createSquareFive())
                     }
                     if (i === 9) {
-                        i++
                         resolve()
                     }
                 }))
@@ -253,7 +259,6 @@ function createSquareSix() {
                         return resolve(createSquareSix())
                     }
                     if (i === 9) {
-                        i++
                         resolve()
                     }
                 }))
@@ -293,7 +298,6 @@ function createSquareSeven() {
                         return resolve(createSquareSeven())
                     }
                     if (i === 9) {
-                        i++
                         resolve()
                     }
                 }))
@@ -332,7 +336,6 @@ function createSquareEight() {
                         return resolve(createSquareEight())
                     }
                     if (i === 9) {
-                        i++
                         resolve()
                     }
                 }))
@@ -371,7 +374,6 @@ function createSquareNine() {
                         return resolve(createSquareNine())
                     }
                     if (i === 9) {
-                        i++
                         resolve()
                     }
                 }))
@@ -379,6 +381,7 @@ function createSquareNine() {
     })
 }
 
+// Fresh empty arrays. Generating new Sudoku table
 async function restart() {
     squareOne = []
     squareTwo = []
